a4ed72c37fd5b1af3ee48f6fd46a0c083ecafae6
moraes.neto <moraesfelicioneto@hotmail.com>
2021-05-27 16:27:54 -0300

    atualizando banco de dados e adicionando novos componentes na aplicação

--- frontend/src/app/autenticacao/autenticacao.service.ts
+++ frontend/src/app/autenticacao/autenticacao.service.ts
@@ -1,9 +1,13 @@
+import { environment } from './../../environments/environment.prod';
 import { UsuarioService } from './usuario/usuario.service';
 import { Usuario } from './usuario/usuario';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +21,7 @@ export class AutenticacaoService {
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
 
-    return this.httpClient.post('http://localhost:3000/user/login', {
+    return this.httpClient.post(`${API}/user/login`, {
       userName: usuario,
       password: senha,
     },
