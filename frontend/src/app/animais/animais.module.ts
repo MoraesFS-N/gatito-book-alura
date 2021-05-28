e11ce5f23588562d82c35553f7ab3d35f9337ac9
moraes.neto <moraesfelicioneto@hotmail.com>
2021-05-28 15:53:40 -0300

    implementação de modules shared

--- frontend/src/app/animais/animais.module.ts
+++ frontend/src/app/animais/animais.module.ts
@@ -1,4 +1,3 @@
-import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
 import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
@@ -9,7 +8,7 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,8 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AnimaisRoutingModule,
     CartaoModule,
-    MensagemModule,
-    ReactiveFormsModule,
+    SharedModule
   ],
 })
 export class AnimaisModule {}
